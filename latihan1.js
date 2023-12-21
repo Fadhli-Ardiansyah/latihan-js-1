ec6321f6f9d72d7db5131710759d506e9ce29164
fadhli ardiansyah <oradila9@gmail.com>
2023-12-21 13:30:43 +0700

    baru

---
+++ latihan1.js
@@ -0,0 +1,21 @@
+alert("mengisi data diri");
+var nama = prompt("nama anda siapa");
+var usia = prompt("masukan usia anda");
+var tinggiBadan = prompt("berapa tinggi badan anda");
+var beratBadan = prompt('berapa berat badan anda')
+
+
+alert(
+    ` nama anda ${nama} dan usia anda ${usia} tinggi badan anda ${tinggiBadan} cm dan berat badan anda ${beratBadan} kg` 
+    );
+    
+    alert('terimakasih sudah mengisi :>')
+// alert(
+//   "nama anda " +
+//     nama +
+//     " dan usia anda " +
+//     usia +
+//     " tinggi badan anda " +
+//     tinggiBadan +
+//     " cm  "
+// );
